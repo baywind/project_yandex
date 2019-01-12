2f6c72d15abc7e32fd736c444ec6e50a69ee2beb
baywind <ZSE$3edc>
2019-01-12 14:51:26 +0300

    extracted Board, created Life

---
+++ gameboard.py
@@ -0,0 +1,48 @@
+import pygame
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[None] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                self.draw_cell(x, y, screen)
+
+    def draw_cell(self, x, y, screen):
+        pygame.draw.rect(screen, pygame.Color(255, 255, 255), self.cell_rect(x, y), 1)
+
+    def cell_rect(self, x, y):
+        return pygame.Rect(x * self.cell_size + self.left, y * self.cell_size + self.top,
+                          self.cell_size, self.cell_size)
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    # cell - кортеж (x, y)
+    def on_click(self, cell):
+        # заглушка для реальных игровых полей
+        pass
+
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
+            return None
+        return cell_x, cell_y
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell and cell < (self.width, self.height):
+            self.on_click(cell)
---
+++ life.py
@@ -0,0 +1,37 @@
+import pygame
+from gena.gameboard import Board
+
+pygame.init()
+size = 820, 820
+screen = pygame.display.set_mode(size)
+
+
+class Life(Board):
+    def on_click(self, cell):
+        x, y  = cell
+        val = self.board[x][y]
+        self.board[x][y] = 0 if val else 1
+
+    def draw_cell(self, x, y, screen):
+        val = self.board[x][y]
+        pygame.draw.rect(screen, pygame.Color(255, 255, 255), self.cell_rect(x, y),
+                         0 if val else 1)
+
+
+board = Life(40, 40)
+board.set_view(10, 10, 20)
+
+running = True
+while running:
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        running = False
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        board.get_click(event.pos)
+
+    screen.fill((0, 0, 0))
+    board.render(screen)
+    pygame.display.flip()
+    # clock.tick(10)
+
+pygame.quit()
